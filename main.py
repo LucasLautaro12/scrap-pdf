c7df386ba42a9e6293a0d2070a85abe7208a7b7f
Quiroga Lucas Lautaro <42857468@fi.unju.edu.ar>
2025-05-21 12:16:27 -0300

    comentarios

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from gui.ventana_principal import iniciar_app
-from updater.updater import check_and_update
+""" from updater.updater import check_and_update """
 
 def main():
-    check_and_update()
+    """ check_and_update() """
     iniciar_app()
 
 if __name__ == "__main__":
